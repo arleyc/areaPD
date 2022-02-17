598bb0639efb20e5822dfe0de5c3c173ab5dba1c
Arley Camargo <arley.camargo@gmail.com>
2022-02-17 09:26:25 -0300

    update multiareaPD

--- R/multiareaPD.R
+++ R/multiareaPD.R
@@ -23,7 +23,7 @@
 #'homoPD<-multiareaPD(homodata,homotree)
 #'
 
-multiareaPD <- function(x, phy) {
+multiareaPD <- function(x, phy, plot=F) {
 
   # check dependencies
 
@@ -37,92 +37,99 @@ multiareaPD <- function(x, phy) {
 
   # calculate area PD
   x <- x[1:5,]
-  PDvals <- picante::pd(x,phy)
-  PD12 <- picante::pd(x[1,]+x[2,],phy)
-  PD13 <- picante::pd(x[1,]+x[3,],phy)
-  PD14 <- picante::pd(x[1,]+x[4,],phy)
-  PD15 <- picante::pd(x[1,]+x[5,],phy)
-  PD23 <- picante::pd(x[2,]+x[3,],phy)
-  PD24 <- picante::pd(x[2,]+x[4,],phy)
-  PD25 <- picante::pd(x[2,]+x[5,],phy)
-  PD34 <- picante::pd(x[3,]+x[4,],phy)
-  PD35 <- picante::pd(x[3,]+x[5,],phy)
-  PD45 <- picante::pd(x[4,]+x[5,],phy)
-  PD123 <- picante::pd(x[1,]+x[2,]+x[3,],phy)
-  PD124 <- picante::pd(x[1,]+x[2,]+x[4,],phy)
-  PD125 <- picante::pd(x[1,]+x[2,]+x[5,],phy)
-  PD134 <- picante::pd(x[1,]+x[3,]+x[4,],phy)
-  PD135 <- picante::pd(x[1,]+x[3,]+x[5,],phy)
-  PD145 <- picante::pd(x[1,]+x[4,]+x[5,],phy)
-  PD234 <- picante::pd(x[2,]+x[3,]+x[4,],phy)
-  PD235 <- picante::pd(x[2,]+x[3,]+x[5,],phy)
-  PD245 <- picante::pd(x[2,]+x[4,]+x[5,],phy)
-  PD345 <- picante::pd(x[3,]+x[4,]+x[5,],phy)
-  PD1234 <- picante::pd(x[1,]+x[2,]+x[3,]+x[4,],phy)
-  PD1235 <- picante::pd(x[1,]+x[2,]+x[3,]+x[5,],phy)
-  PD1245 <- picante::pd(x[1,]+x[2,]+x[4,]+x[5,],phy)
-  PD1345 <- picante::pd(x[1,]+x[3,]+x[4,]+x[5,],phy)
-  PD2345 <- picante::pd(x[2,]+x[3,]+x[4,]+x[5,],phy)
-  PD12345 <- picante::pd(x[1,]+x[2,]+x[3,]+x[4,]+x[5,],phy)
 
-  # find the tips for the area
-  tips<-colnames(x)
+  results<-c()
 
-  PDpath<-vector("list", dim(x)[1])
+  for (i in 1:length(phy)) {
+    PDvals <- picante::pd(x,phy[i])
+    PD12 <- picante::pd(x[1,]+x[2,],phy[i])
+    PD13 <- picante::pd(x[1,]+x[3,],phy[i])
+    PD14 <- picante::pd(x[1,]+x[4,],phy[i])
+    PD15 <- picante::pd(x[1,]+x[5,],phy[i])
+    PD23 <- picante::pd(x[2,]+x[3,],phy[i])
+    PD24 <- picante::pd(x[2,]+x[4,],phy[i])
+    PD25 <- picante::pd(x[2,]+x[5,],phy[i])
+    PD34 <- picante::pd(x[3,]+x[4,],phy[i])
+    PD35 <- picante::pd(x[3,]+x[5,],phy[i])
+    PD45 <- picante::pd(x[4,]+x[5,],phy[i])
+    PD123 <- picante::pd(x[1,]+x[2,]+x[3,],phy[i])
+    PD124 <- picante::pd(x[1,]+x[2,]+x[4,],phy[i])
+    PD125 <- picante::pd(x[1,]+x[2,]+x[5,],phy[i])
+    PD134 <- picante::pd(x[1,]+x[3,]+x[4,],phy[i])
+    PD135 <- picante::pd(x[1,]+x[3,]+x[5,],phy[i])
+    PD145 <- picante::pd(x[1,]+x[4,]+x[5,],phy[i])
+    PD234 <- picante::pd(x[2,]+x[3,]+x[4,],phy[i])
+    PD235 <- picante::pd(x[2,]+x[3,]+x[5,],phy[i])
+    PD245 <- picante::pd(x[2,]+x[4,]+x[5,],phy[i])
+    PD345 <- picante::pd(x[3,]+x[4,]+x[5,],phy[i])
+    PD1234 <- picante::pd(x[1,]+x[2,]+x[3,]+x[4,],phy[i])
+    PD1235 <- picante::pd(x[1,]+x[2,]+x[3,]+x[5,],phy[i])
+    PD1245 <- picante::pd(x[1,]+x[2,]+x[4,]+x[5,],phy[i])
+    PD1345 <- picante::pd(x[1,]+x[3,]+x[4,]+x[5,],phy[i])
+    PD2345 <- picante::pd(x[2,]+x[3,]+x[4,]+x[5,],phy[i])
+    PD12345 <- picante::pd(x[1,]+x[2,]+x[3,]+x[4,]+x[5,],phy[i])
 
-  for (i in 1:dim(x)[1]) {
-    areatips<-tips[x[i,]>0]
+    # find the tips for the area
+    tips<-colnames(x)
 
-    # find label number for area tips
-    mytips<-match(areatips,phy$tip.label)
+    PDpath<-vector("list", dim(x)[1])
 
-    # find edges corresponding to tips
-    myedges<-match(mytips,phy$edge[,2])
+    for (j in 1:dim(x)[1]) {
+      areatips<-tips[x[j,]>0]
 
-    # when there is a single tip
-    if (length(myedges)==1) {
-      newedge<-c(1:dim(phy$edge)[1])[phy$edge[myedges,][1]==phy$edge[,2]]
-      while (length(newedge)==1) {
-        myedges<-c(myedges,newedge)
-        newedge<-c(1:dim(phy$edge)[1])[phy$edge[newedge,][1]==phy$edge[,2]]
+      # find label number for area tips
+      mytips<-match(areatips,phy[i]$tip.label)
+
+      # find edges corresponding to tips
+      myedges<-match(mytips,phy[i]$edge[,2])
+
+      # when there is a single tip
+      if (length(myedges)==1) {
+        newedge<-c(1:dim(phy[i]$edge)[1])[phy[i]$edge[myedges,][1]==phy[i]$edge[,2]]
+        while (length(newedge)==1) {
+          myedges<-c(myedges,newedge)
+          newedge<-c(1:dim(phy[i]$edge)[1])[phy[i]$edge[newedge,][1]==phy[i]$edge[,2]]
+        }
       }
-    }
 
-    # when there are 2 or more tips
-    if (length(myedges)>1){
+      # when there are 2 or more tips
+      if (length(myedges)>1){
 
-      # find ancestral edges to paint path to root
-      newedges<-match(phy$edge[myedges,][,1],phy$edge[,2])
-      newedges <- newedges[!is.na(newedges)]
-
-      # add ancestral edges to the list of edges to paint
-      # myedges<-c(myedges,newedges)
-
-      # repeat finding more ancestral edges and re-paint
-      while (length(newedges)>1) {
-        myedges<-c(myedges,newedges)
-        newedges<-match(phy$edge[newedges,][,1],phy$edge[,2])
+        # find ancestral edges to paint path to root
+        newedges<-match(phy[i]$edge[myedges,][,1],phy[i]$edge[,2])
         newedges <- newedges[!is.na(newedges)]
+
+        # add ancestral edges to the list of edges to paint
+        # myedges<-c(myedges,newedges)
+
+        # repeat finding more ancestral edges and re-paint
+        while (length(newedges)>1) {
+          myedges<-c(myedges,newedges)
+          newedges<-match(phy[i]$edge[newedges,][,1],phy[i]$edge[,2])
+          newedges <- newedges[!is.na(newedges)]
+        }
       }
+      PDpath[[j]]<-myedges
     }
-    PDpath[[i]]<-myedges
-  }
 
   # plots PDpaths for each area
 
-  graphics::par(mfrow=c(3,2), mar=c(0, 0, 1, 0))
+  if (plot==T) {
 
-  for (i in 1:dim(x)[1]) {
+    graphics::par(mfrow=c(3,2), mar=c(0, 0, 1, 0))
 
-    # create vector of edge colors
-    edgecols<-rep("black",dim(phy$edge)[1])
+    for (i in 1:dim(x)[1]) {
 
-    # change colors of myedges
-    edgecols[PDpath[[i]]]<-c("red")
+      # create vector of edge colors
+      edgecols<-rep("black",dim(phy$edge)[1])
 
-    # plot tree with painted terminal branches
-    ape::plot.phylo(phy,edge.color=edgecols,edge.width =3, use.edge.length=T)
-    graphics::title(main=paste("PD Area",rownames(x[i,]),"=",PDvals[i,1]))
+      # change colors of myedges
+      edgecols[PDpath[[i]]]<-c("red")
+
+      # plot tree with painted terminal branches
+      ape::plot.phylo(phy,edge.color=edgecols,edge.width =3, use.edge.length=T)
+      graphics::title(main=paste("PD Area",rownames(x[i,]),"=",PDvals[i,1]))
+    }
   }
 
   # create, save, and plot Venn's diagram
@@ -166,25 +173,32 @@ multiareaPD <- function(x, phy) {
     n23 + n24 + n25 + n34 + n35 + n45 - n123 - n124 - n125 - n134 - n135 - n145 - n234 -
     n235 - n245 - n345 + n1234 + n1235 + n1245 + n1345 + n2345
 
-  # make Venn object without plotting
-  vennfig<-VennDiagram::draw.quintuple.venn(area1,area2,area3,area4,area5,n12,n13,n14,n15,n23,n24,n25,n34
-                               ,n35,n45,n123,n124,n125,n134,n135,n145,n234,n235,n245,n345,n1234,n1235,n1245,n1345,
-                               n2345,n12345,
-                               category=c("TGC","LCL","TCS","TCO","TQS"),
-                               fill = c("dodgerblue", "goldenrod1", "red", "seagreen3", "orchid3"),
-                               cex = c(1.5, 1.5, 1.5, 1.5, 1.5, 1, 0.8, 1, 0.8, 1, 0.8, 1, 0.8, 1, 0.8, 1, 0.55, 1, 0.55,
-                                       1, 0.55, 1, 0.55, 1, 0.55, 1, 1, 1, 1, 1, 1.5),
-                               cat.col = c("dodgerblue", "goldenrod1", "red", "seagreen3", "orchid3"),
-                               cat.cex = 1.5, cat.fontface = "bold", margin = 0.05, ind=F)
 
-  # save Venn plot as jpeg file
-  grDevices::jpeg(filename = "Venn_diagram.jpeg")
-  grid::grid.draw(vennfig)
-  grDevices::dev.off()
-  im<-imager::load.image("Venn_diagram.jpeg")
-  graphics::par(mar=c(1, 1, 1, 1))
-  plot(im,axes=FALSE)
+  if (plot==T) {
+    # make Venn object without plotting
+    vennfig<-VennDiagram::draw.quintuple.venn(area1,area2,area3,area4,area5,n12,n13,n14,n15,n23,n24,n25,n34
+                                 ,n35,n45,n123,n124,n125,n134,n135,n145,n234,n235,n245,n345,n1234,n1235,n1245,n1345,
+                                 n2345,n12345,
+                                 category=c("TGC","LCL","TCS","TCO","TQS"),
+                                 fill = c("dodgerblue", "goldenrod1", "red", "seagreen3", "orchid3"),
+                                 cex = c(1.5, 1.5, 1.5, 1.5, 1.5, 1, 0.8, 1, 0.8, 1, 0.8, 1, 0.8, 1, 0.8, 1, 0.55, 1, 0.55,
+                                         1, 0.55, 1, 0.55, 1, 0.55, 1, 1, 1, 1, 1, 1.5),
+                                 cat.col = c("dodgerblue", "goldenrod1", "red", "seagreen3", "orchid3"),
+                                 cat.cex = 1.5, cat.fontface = "bold", margin = 0.05, ind=F)
 
-  return (c(area1,area2,area3,area4,area5,n12,n13,n14,n15,n23,n24,n25,n34,n35,n45,n123,n124,n125,n134,n135,n145,n234,n235,n245,n345,n1234,n1235,n1245,n1345,n2345,n12345))
+    # save Venn plot as jpeg file
+    grDevices::jpeg(filename = "Venn_diagram.jpeg")
+    grid::grid.draw(vennfig)
+    grDevices::dev.off()
+    im<-imager::load.image("Venn_diagram.jpeg")
+    graphics::par(mar=c(1, 1, 1, 1))
+    plot(im,axes=FALSE)
+    }
+
+    results<-rbind(results,c(area1,area2,area3,area4,area5,n12,n13,n14,n15,n23,n24,n25,n34,n35,n45,n123,n124,n125,n134,n135,n145,n234,n235,n245,n345,n1234,n1235,n1245,n1345,n2345,n12345))
+
+  }
+
+  return(results)
 
 }
